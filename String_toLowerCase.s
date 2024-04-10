344fbf59d9b823adcb5a82e5749c1312e9486bf4
TheQueenOfSauce <maysaucedo849@gmail.com>
2024-04-10 05:23:08 -0700

    added String_toLowerCase.s

--- String_toLowerCase.s
+++ String_toLowerCase.s
@@ -1,21 +1,17 @@
-/* Subroutine String_replace: Replaces all occurences of an oldChar character
- * with newChar character.
- * x0: String
- * x1: Char to replace
- * x2: Char replaced(Deleted)
+/* Subroutine String_toLowerCase: Converts the characters of a
+ * string into  lower case characters.
+ * x0: String to convert to lower case
  * Returns:
- * x0: Address of new character replaced string
+ * x0: Address of new lower case string
  * All AAPCS required registers are preserved
 */
 
-	.global String_replace
+	.global String_toLowerCase
 
 	.text
-String_replace:
-	STP		LR,x0,[SP,#-16]!	// Push return address and modify string
-	STP		x1,x2,[SP,#-16]!  // Push x1 and x2 (x1 - Old char)
-										// 					(x2 - New Char)
-
+String_toLowerCase:
+	STP		LR,x0,[SP,#-16]!	// Push return address
+										// (x0 -String to lower case)
 
 	// Use string length to find length to use as lcv
 
@@ -26,28 +22,25 @@ String_replace:
 
 	bl			malloc				// Ask for memory for newly created
 										// string
-	ldp		x2,x3,[SP],#16		// Pop regis x2 and x3 (Old and new chars)
-	ldp		LR,x1,[SP],#16    //	Pop regis LR and x1(Return address and old char)
+										// Since we have mallo we need to preserve the
+										// link regi
+	ldp		LR,x1,[SP],#16		// Pop regis x1 and link
 	stp		LR,x0,[SP,#-16]!	// Push regis LR and x0(String address)
 
 loop:
 	ldrb		w4,[x1],#1			// Load a single byte(current char and i++)
-	cmp		w4,#0					// if char == \n
-	beq		exit					// if \n branch to exit sequence
-	cmp		w4,w2					// if char == replaceKey
-	beq		replace				// branch to replace
-	// If not char != continue through loop,
-	strb		w4,[x0],#1			// Store a single byte(char)to new string i++
-	b			loop					// Unconditional branch back to loop
-
-
-replace:
-	// Replace chars
-	strb		w3,[x0],#1			// Store a single byte(char) to string and increment
-										// pointer by 1
-	b			loop					// Unconditional branch to loop
-
+	cmp		w4,#'Z'				// Check if there is Z in w4
+	bgt		lower					// If w4 is greater than Z branch to lower
+	cmp		w4,'A'				// Check if there is 'A' in w4
+	blt		lower					// branch to lower  if lower than 'A'
+	// Else if letter is not within the A-Z range
+	// it means it's already lowercase
+	add		w4,w4,#('a'-'A')	// Replace upper with lower case
+lower:
+	strb		w4,[x0],#1			// Store byte(char) to output string
+	cmp		w4,#0					// Compare/check w5 for 0
+	bne		loop					// Unconditional branch to loop back to loop
 
 exit:
-	ldp		LR,x0,[SP],#16		// pop link regi and x0 to stack(Return address and return string address)
-	RET								// Return to driver
+	ldp		LR,x0,[SP],#16		// pop link regi and x0 to stack( return string address)
+	RET								// Return to driver.s
